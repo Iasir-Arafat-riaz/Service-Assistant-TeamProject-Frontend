10eaa6e44631619f713e69fa567c235608bda5d9
Naimur53 <naimurrhman53@gmail.com>
2022-02-10 15:12:28 +0600

    done

--- src/App.js
+++ src/App.js
@@ -6,21 +6,36 @@ import Error from "./Pages/Eroor/Error";
 import Services from "./Pages/Services/Services";
 import Dashboard from "./Pages/Dashboard/Dashboard";
 import ContactUs from "./Pages/ContactUs/ContactUs";
+import { useEffect } from 'react'
+import io from 'socket.io-client'
+
+// made a socket with server
+const socket = io('http://localhost:5000');
+
 
 function App() {
+
+  // connecting the server 
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log('connection done!')
+      socket.send("Hello!");
+    });
+  }, []);
+
   return (
     <BrowserRouter>
       <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Home" element={<Home />} />
-        <Route path="/SERVICES" element={<Services/>} />
-        <Route path="/Dashboard" element={<Dashboard/>} />
-        <Route path="/Contact" element={<ContactUs/>} />
+        <Route path="/SERVICES" element={<Services />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/Contact" element={<ContactUs />} />
         <Route path="*" element={<Error />} />
 
       </Routes>
-     
+
     </BrowserRouter>
   );
 }
