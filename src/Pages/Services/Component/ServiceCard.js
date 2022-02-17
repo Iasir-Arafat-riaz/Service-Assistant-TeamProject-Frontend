490b8b27b0827dc258384e1e1059b2ecb8d4bad6
mdsagar333 <md_sagar@yahoo.com>
2022-02-17 23:35:42 +0600

    service card added

---
+++ src/Pages/Services/Component/ServiceCard.js
@@ -0,0 +1,32 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+
+const ServiceCard = ({ Name, Img, Id }) => {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={Img}
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {Name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
+
+export default ServiceCard;
--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,9 +1,84 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Grid } from "@mui/material";
+import { Container } from "@mui/material";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemButton from "@mui/material/ListItemButton";
+import { makeStyles } from "@mui/styles";
+import { loadServiceCategory } from "../../redux/Reducers/reducersSagar/servicesSlice";
+import ServiceCard from "./Component/ServiceCard";
+
+const useStyles = makeStyles({
+  drawerPaper: {
+    marginTop: "100px",
+  },
+});
 
 const Services = () => {
+  const dispatch = useDispatch();
+  const { allServices, serviceIsLoading } = useSelector(
+    (state) => state.serviceCategoryState
+  );
+  const classes = useStyles();
+
+  const drawerWidth = 240;
+
+  useEffect(() => {
+    const service = dispatch(loadServiceCategory());
+  }, []);
+
+  if (serviceIsLoading) {
+    return <h1 style={{ marginTop: "100px" }}>Loading....</h1>;
+  }
+
+  let menuItem;
+  // if (allServices.length > 0){
+  //   menuItem = allServices.map(ite )
+  // }
+
   return (
     <div style={{ marginTop: "100px" }}>
-      <h1>Our All Services</h1>
+      <h1>Our All Services sagar</h1>
+      <Container>
+        <Grid container>
+          <Grid item>
+            <Drawer
+              sx={{ width: drawerWidth }}
+              variant="permanent"
+              anchor="left"
+              classes={{
+                paper: classes.drawerPaper,
+              }}
+            >
+              <List>
+                {allServices.map((item) => {
+                  return (
+                    <ListItem key={item._id}>
+                      <ListItemButton>
+                        <ListItemText>{item.Category}</ListItemText>
+                      </ListItemButton>
+                    </ListItem>
+                  );
+                })}
+              </List>
+            </Drawer>
+          </Grid>
+          <Grid item>
+            <Grid container>
+              {allServices.map((service) => (
+                <Grid container key={`${service._id}${service.Category}`}>
+                  {service.Services.map((item) => (
+                    <ServiceCard key={item.Id} {...item} />
+                  ))}
+                </Grid>
+              ))}
+            </Grid>
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 };
