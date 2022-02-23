0feecb205bf54d9809e6f9f4ffcf4ac11b2beef4
Iasir-Arafat-riaz <76946854+Iasir-Arafat-riaz@users.noreply.github.com>
2022-02-23 06:13:00 +0600

    addBanner section added

--- src/Pages/Dashboard/DashboardPages/AddBanner/AddBanner.js
+++ src/Pages/Dashboard/DashboardPages/AddBanner/AddBanner.js
@@ -1,86 +1,13 @@
-import {
-  Autocomplete,
-  Box,
-  Button,
-  Grid,
-  Paper,
-  Stack,
-  TextField,
-  Typography,
-} from "@mui/material";
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-import PreviewHeader from "../../../Home/Header/PreviewHeader";
-import "./AddBanner.css";
+import React from 'react';
+import AddBannerSection from './AddBannerSection/AddBannerSection';
 
 const AddBanner = () => {
-   
-  const {
-    register,
-    handleSubmit,
-    reset,
-    watch,
-    
-  } = useForm();
-
-  const bannerInfo = 
-    {
-        imageUrl: watch("imageUrl"),
-        bannerText: watch("bannerText"),
-        quality: watch("quality"),
-    }
- 
- console.log(bannerInfo);
-
-  const onSubmit = (data) => {
-    console.log(data);
-    // reset();
-  };
-
-//   console.log(datas);
-
   return (
-    <Box>
-       
-      <Grid container spacing={2} alignItems="center" justifyContent="center">
-          
-        <Grid item xs={12} md={6}>
-         <PreviewHeader bannerInfo={bannerInfo}/>
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Paper elevation={3} sx={{ p: 2 }}>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <Stack direction="column" elevation={2}>
-                <TextField
-                  required
-                  type="text"
-                  sx={{ mb: 3 }}
-                  {...register("imageUrl")}
-                  label="Enter image url"
-                  variant="standard"
-                />
-                <TextField
-                  required
-                  type="text"
-                  sx={{ mb: 3 }}
-                  {...register("bannerText")}
-                  label="Write Banner text"
-                  variant="standard"
-                />
-                <select id="formOption" {...register("quality")}>
-                  <option value="High Quality Banner">HighQuality</option>
-                  <option value="Low Quality Banner">LowQuality</option>
-                  <option value="Normal Quality Banner">Normal</option>
-                </select>
-                <Button type="submit" variant="contained">
-                  Add Banner
-                </Button>
-              </Stack>
-            </form>
-          </Paper>
-        </Grid>
-      </Grid>
-    </Box>
+    <div>
+      <AddBannerSection/>
+      <AddBannerSection/>
+      <AddBannerSection/>
+    </div>
   );
 };
 
