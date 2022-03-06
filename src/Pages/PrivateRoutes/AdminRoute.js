322d2f6f55c99aa13f16d3d2a1bc39e53e96fe0c
unknown <mdrakibulhoque83@gmail.com>
2022-03-06 19:00:57 +0600

    Admin private route create

--- src/Pages/PrivateRoutes/AdminRoute.js
+++ src/Pages/PrivateRoutes/AdminRoute.js
@@ -1,18 +1,18 @@
-// import React from 'react';
-// import { useSelector } from 'react-redux';
-// import { Navigate, useLocation } from 'react-router-dom';
-// import { allData } from '../../redux/dataSlice/dataSlice';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Navigate, useLocation } from 'react-router-dom';
+import { allData } from '../../redux/dataSlice/dataSlice';
 
 
-// const AdminRoute = ({ children, ...rest }) => {
-//     const { allUser, user } = useSelector(allData);
-//     console.log("user role", user.role);
-//     const location = useLocation();
+const AdminRoute = ({ children, ...rest }) => {
+    const { allUser, user } = useSelector(allData);
+    console.log("user role", user.role);
+    const location = useLocation();
 
-//     if (user?.email && allUser.filter((user) => user?.role === "admin")) {
-//         return children;
-//     }
-//     return <Navigate to="/home" state={{ from: location }} />;
+    if (user?.email && allUser.filter((user) => user?.role === "admin")) {
+        return children;
+    }
+    return <Navigate to="/home" state={{ from: location }} />;
 
-// };
-// export default AdminRoute;
+};
+export default AdminRoute;
