dd72296954a1cb1f58a38b8c8d4029f064784d01
Md. Kawsar Hossain <kawsarm104@gmail.com>
2022-03-07 23:11:56 +0600

    changes in card in add provider

--- src/Pages/AllProvider/AllProviderChild.js
+++ src/Pages/AllProvider/AllProviderChild.js
@@ -1,14 +1,20 @@
 import React from 'react';
 // material ui card start 
 import Card from '@mui/material/Card';
+import Rating from '@mui/material/Rating';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import Avatar from '@mui/material/Avatar';
 
+import CardHeader from '@mui/material/CardHeader';
+
+import IconButton from '@mui/material/IconButton';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
 const AllProviderChild = (props) => {
-    const {displayName,email,image, photoURL, role, createdAt,uid,_id} = props.provider
+    const {displayName,email,image, photoURL, role,rating, createdAt,uid,_id} = props.provider
     return (
         <Card  sx={{ml:2}}>
         <CardMedia
@@ -24,24 +30,35 @@ const AllProviderChild = (props) => {
   sx={{ width: 50, height: 50, mt: -3, ml:2 }}
 />
         <CardContent>
+       
           <Typography gutterBottom variant="h6" component="div">
             {displayName} 
           </Typography>
           <Typography gutterBottom variant="p" component="div">
-           <span style={{backgroundColor:"#28a745", padding:2, color:"white"}}>Verified</span>
+           <span style={{backgroundColor:"#ff7043", padding:2, color:"white"}}>Featured</span>
+           <span style={{backgroundColor:"#28a745", padding:2, color:"white", marginLeft:"7px"}}>Verified</span>
           </Typography>
-          {/* <Typography gutterBottom variant="p" component="div">
-            {role}
-          </Typography> */}
-          {/* <Typography variant="body2" color="text.secondary">
-            {textArea?.split(' ').slice(0, 8).toString().replace(/,/g, ' space')}
-          </Typography> */}
+          <Typography gutterBottom variant="p" component="div">
+          <Rating name="read-only" value={rating} readOnly />
+          </Typography>
+          <Typography  variant="h6" color="text.primary" component="div">
+          A Place Where We Care Life
+          </Typography>
         </CardContent>
-        <CardActions className="d-flex justify-content-end">
+        {/* <CardActions className="d-flex justify-content-end"> */}
           {/* <Link to={`/dashboard/alladvocates/${_id}`} style={{ textDecoration: "none" }}>
             <Button size="small" className="">View Details</Button>
           </Link> */}
-        </CardActions>
+           <CardHeader
+       
+       action={
+         <IconButton aria-label="settings">
+           <MoreVertIcon />
+         </IconButton>
+       }
+    
+     />
+        {/* </CardActions> */}
       </Card>
     );
 };
