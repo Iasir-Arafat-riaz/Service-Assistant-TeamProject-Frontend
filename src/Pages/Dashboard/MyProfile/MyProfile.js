d5fc9dab0fc185853cf511ffa6cc6861077d3ba8
Md. Kawsar Hossain <kawsarm104@gmail.com>
2022-03-09 11:36:01 +0600

    collecting data for my profile

--- src/Pages/Dashboard/MyProfile/MyProfile.js
+++ src/Pages/Dashboard/MyProfile/MyProfile.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
+import { useSelector } from 'react-redux';
+import { allData}  from '../../../redux/dataSlice/dataSlice'
+import axios from 'axios'
 
 const MyProfile = () => {
+    const [profile, setProfile] = useState(null)
+    const { user } = useSelector(allData);
+
+    useEffect(() => {
+        const api = `http://localhost:5000/users/${user.email}`
+        axios.get(api).then(res => {
+            console.log(res)
+            setProfile(res.data)
+            console.log(res.data,"== got user profile")
+        })
+    }, []);
+    
     return (
         <div>
-            My Profile .... ... .... ... ...... .. 
+            My Profile .... ... .... ... ...... ..  {user.email}
         </div>
     );
 };
