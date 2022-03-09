0a62780c6fa9e12cbbc60a04ace7d9e0cf7c799e
Md. Kawsar Hossain <kawsarm104@gmail.com>
2022-03-09 12:53:34 +0600

    fixed image issue

--- src/Pages/Dashboard/MyProfile/MyProfile.js
+++ src/Pages/Dashboard/MyProfile/MyProfile.js
@@ -2,6 +2,16 @@ import React,{useState, useEffect} from 'react';
 import { useSelector } from 'react-redux';
 import { allData}  from '../../../redux/dataSlice/dataSlice'
 import axios from 'axios'
+// material ui card start 
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Avatar from '@mui/material/Avatar';
+
+import IconButton from '@mui/material/IconButton';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { Box, CardHeader } from '@mui/material';
 
 const MyProfile = () => {
     const [profile, setProfile] = useState(null)
@@ -14,12 +24,49 @@ const MyProfile = () => {
             setProfile(res.data)
             console.log(res.data,"== got user profile")
         })
-    }, []);
+    }, [user?.email]);
     
     return (
-        <div>
-            My Profile .... ... .... ... ...... ..  {user.email}
-        </div>
+        <Card  sx={{}}>
+        <CardMedia
+          component="img"
+          style={{width:"100%",marginX:"30%"}}
+          height="250"
+          image={profile?.image}
+          alt="user background pic"
+        />
+        <Avatar
+  alt={user.displayName}
+  src={user.photoURL}
+  sx={{ width: 50, height: 50, mt: -3, ml:2 }}
+/>
+        
+        <CardContent>
+       
+          <Typography gutterBottom variant="h6" component="div">   
+            {user.displayName} 
+          </Typography>
+          <Typography gutterBottom variant="h6" component="div">
+            {user.email} 
+          </Typography>
+  
+
+          <Typography gutterBottom variant="h6" component="div">
+            {user.role} 
+          </Typography>
+          <Typography gutterBottom variant="h6" component="div">
+            {user.name} 
+          </Typography>
+       
+
+        </CardContent>
+        
+        
+          <>
+ 
+
+</>
+      </Card>
     );
 };
 
