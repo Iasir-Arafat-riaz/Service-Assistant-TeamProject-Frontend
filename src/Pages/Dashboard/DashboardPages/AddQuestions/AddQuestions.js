70aa29b896b6179e0e0e461ecc8f65edf8fd9b47
Md. Kawsar Hossain <kawsarm104@gmail.com>
2022-03-16 06:58:17 +0600

    added two input field

--- src/Pages/Dashboard/DashboardPages/AddQuestions/AddQuestions.js
+++ src/Pages/Dashboard/DashboardPages/AddQuestions/AddQuestions.js
@@ -1,10 +1,20 @@
 import React from 'react';
-
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 const AddQuestions = () => {
     return (
-        <div>
-            add your sample questions here ... 
-        </div>
+        <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+
+      <TextField id="standard-basic" label="Add Question" variant="standard" />
+      <TextField id="standard-basic" label="Add Answer" variant="standard" />
+    </Box>
     );
 };
 
