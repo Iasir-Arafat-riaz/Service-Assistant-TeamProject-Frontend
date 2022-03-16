57d33dc378174d4da6f00501d349db48e2be561b
Md. Kawsar Hossain <kawsarm104@gmail.com>
2022-03-16 07:15:10 +0600

    hoook form working

--- src/Pages/Dashboard/DashboardPages/AddQuestions/AddQuestions.js
+++ src/Pages/Dashboard/DashboardPages/AddQuestions/AddQuestions.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import { useForm } from "react-hook-form";
 const AddQuestions = () => {
+
+    // React Hook form 
+    const { register, handleSubmit,reset, formState: { errors } } = useForm();
+    const onSubmit = data => {
+        console.log(data)
+        reset()
+    };
+  
+
     return (
-        <Box
+        <form onSubmit={handleSubmit(onSubmit)}>
+         <Box
       component="form"
       sx={{
         '& > :not(style)': { m: 1, width: '25ch' },
@@ -12,9 +23,13 @@ const AddQuestions = () => {
       autoComplete="off"
     >
 
-      <TextField id="standard-basic" label="Add Question" variant="standard" />
-      <TextField id="standard-basic" label="Add Answer" variant="standard" />
-    </Box>
+      <TextField {...register("addQuestion", { required: true })}id="standard-basic"  label="Add Question" variant="standard" />
+       <TextField {...register("addAnswer", { required: true })} id="standard-basic" label="Add Answer" variant="standard" />
+  
+     </Box>
+     
+     <input type="submit" />
+   </form>
     );
 };
 
