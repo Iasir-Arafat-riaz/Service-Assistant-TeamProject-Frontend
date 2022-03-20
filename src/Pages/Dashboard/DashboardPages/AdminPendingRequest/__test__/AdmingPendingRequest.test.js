8e59d28ef90782743834862b2a5a0ed19a840fdb
mdsagar333 <md_sagar@yahoo.com>
2022-03-21 02:19:51 +0600

    test case added for admin pending request page

---
+++ src/Pages/Dashboard/DashboardPages/AdminPendingRequest/__test__/AdmingPendingRequest.test.js
@@ -0,0 +1,6 @@
+import { render, screen } from "@testing-library/react";
+import AdminPendingRequest from "../AdminPendingRequest";
+
+test("Testing admin pending request page", () => {
+  render(<AdminPendingRequest />);
+});
