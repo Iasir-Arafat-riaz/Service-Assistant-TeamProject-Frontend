a2c1406174483894502db4ac77b0d621a91daedb
unknown <mdrakibulhoque83@gmail.com>
2022-03-22 01:45:45 +0600

    date added on card

--- src/Pages/Dashboard/DashboardPages/ServiceProvider/Appointment/AppointmentRequest.js
+++ src/Pages/Dashboard/DashboardPages/ServiceProvider/Appointment/AppointmentRequest.js
@@ -22,7 +22,7 @@ const AppointmentRequest = () => {
     const { user } = useSelector(allData)
 
     useEffect(() => {
-        axios.get(`https://dry-sea-00611.herokuapp.com/provider/appointment/${user.email}`)
+        axios.get(`https://dry-sea-00611.herokuapp.com/provider/appointment/naimurrhman53@gmail.com`)
             .then(res => {
                 setProviderEmail(res.data)
                 console.log("provider email", res.data)
@@ -40,6 +40,7 @@ const AppointmentRequest = () => {
                         // >
 
                         // </AppointmentDetails>
+                        
                         <Grid item xs={12} md={4} lg={4}>
                             <Card sx={{mb: 3}}>
                                 <Box sx={{ display: 'flex', justifyContent: 'center', boxShadow: 3, }}>
@@ -62,6 +63,8 @@ const AppointmentRequest = () => {
                                         <Typography className={classes.root}>Customer: <span>{appointment.orderInfo.name}</span></Typography>
                                         <Typography className={classes.root}>Contact:  <Chip label={appointment.orderInfo.number} size="small" variant="outlined" /></Typography>
                                         <Typography className={classes.root}>Address: <span>{appointment.orderInfo.address}</span></Typography>
+                                        <Typography className={classes.root}>Order Date: <span>{new Date(appointment?.date).toLocaleDateString()}</span></Typography>
+                                        
                                     </CardContent>
                                 </CardActionArea>
                             </Card>
