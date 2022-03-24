9df58de23c1ec7deb3e750e44239961875d40de7
unknown <mdrakibulhoque83@gmail.com>
2022-03-24 12:41:35 +0600

    saved service  payment partyJs implement

--- src/Pages/Dashboard/SavedServices/PaymentService/PaymentServiceCard.js
+++ src/Pages/Dashboard/SavedServices/PaymentService/PaymentServiceCard.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { allData, sendNotification } from '../../../../redux/dataSlice/dataSlice';
-
+import Particles from "react-tsparticles";
 
 
 
@@ -96,6 +96,130 @@ const PaymentServiceCard = ({ orderService, handleNextStep }) => {
         };
     };
 
+    const particlesInit = (main) => {
+        console.log(main);
+
+        // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+    };
+
+    const particlesLoaded = (container) => {
+        console.log(container);
+    };
+
+    const options = {
+        background: {
+            color: {
+                value: "#fff"
+            }
+        },
+        fullScreen: {
+            enable: true,
+            zIndex: -1
+        },
+        interactivity: {
+            detectsOn: "window"
+        },
+        emitters: {
+            position: {
+                x: 50,
+                y: 50
+            },
+            rate: {
+                quantity: 15,
+                delay: 0.25
+            }
+        },
+        particles: {
+            color: {
+                value: ["#1E00FF", "#FF0061", "#E1FF00", "#00FF9E"]
+            },
+            move: {
+                decay: 0.05,
+                direction: "top",
+                enable: true,
+                gravity: {
+                    enable: true,
+                    maxSpeed: 150
+                },
+                outModes: {
+                    top: "none",
+                    default: "destroy"
+                },
+                speed: { min: 25, max: 50 }
+            },
+            number: {
+                value: 0
+            },
+            opacity: {
+                value: 1
+            },
+            rotate: {
+                value: {
+                    min: 0,
+                    max: 360
+                },
+                direction: "random",
+                animation: {
+                    enable: true,
+                    speed: 30
+                }
+            },
+            tilt: {
+                direction: "random",
+                enable: true,
+                value: {
+                    min: 0,
+                    max: 360
+                },
+                animation: {
+                    enable: true,
+                    speed: 30
+                }
+            },
+            size: {
+                value: 8
+            },
+            roll: {
+                darken: {
+                    enable: true,
+                    value: 25
+                },
+                enable: true,
+                speed: {
+                    min: 5,
+                    max: 15
+                }
+            },
+            wobble: {
+                distance: 30,
+                enable: true,
+                speed: {
+                    min: -7,
+                    max: 7
+                }
+            },
+            shape: {
+                type: [
+                    "circle",
+                    "square",
+                    "polygon",
+                    "rectangle",
+        
+                ],
+                options: {
+                    polygon: [
+                        {
+                            sides: 5
+                        },
+                        {
+                            sides: 6
+                        }
+                    ],
+                  
+                }
+            }
+        }
+    }
 
 
     return (
@@ -127,7 +251,17 @@ const PaymentServiceCard = ({ orderService, handleNextStep }) => {
                         Pay $ {price}
                     </button>}
 
-                    {success && <Alert sx={{ mt: 2, mb: 2 }} severity="success">{success}</Alert>}
+                    {success && 
+                    <>
+                    <Alert sx={{ mt: 2, mb: 2 }} severity="success">{success}</Alert>
+                    <Particles
+                                id="tsparticles"
+                                init={particlesInit}
+                                loaded={particlesLoaded}
+                                options={options}
+                            />
+                    </>
+                    }
                     {error && <Alert severity="error">{error}</Alert>
                     }
                 </Paper>
